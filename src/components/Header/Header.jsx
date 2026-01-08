783d3b5a74223fb51cfbd0d452eb165458b5bf56
ImadAlam21 <imadalam2112@gmail.com>
2026-01-08 19:54:18 +0530

    bug fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -71,7 +71,7 @@ export default function Head() {
                 <div onClick={handleLogoClick}
                     className="cursor-pointer">
                     <img
-                        className="w-20 lg:w-30 aspect-auto"
+                        className="w-30 lg:w-35 aspect-auto"
                         src="/logo/logo.png" alt="Appit Logo" />
                 </div>
 
@@ -117,7 +117,7 @@ export default function Head() {
                             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     ) : (
-                        <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 md:w-6 md:h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                         </svg>
                     )}
